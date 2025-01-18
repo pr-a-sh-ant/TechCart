366589726fbe04501b083e0f1522c23841465d59
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-01-18 20:08:57 +0545

    recreated folder

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CredentialSignIn from '../components/sign-up'
+const Login = () => {
+  return (
+    <div >
+      <CredentialSignIn />
+    </div>
+  )
+}
+
+export default Login
