543a1e8350ef809b0224997a0736575a77165721
pr-a-sh-ant <prashantaryal07@gmail.com>
2025-02-22 14:57:38 +0545

    updated cloudinary signin

--- server/routes/cloudinarySign.js
+++ server/routes/cloudinarySign.js
@@ -5,15 +5,20 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const cloudinary = require("cloudinary");
 
-router.post("/", (req, res) => {
-  const timestamp = Math.round(new Date().getTime() / 1000);
-  const signature = cloudinary.utils.api_sign_request(
-    {
-      timestamp: timestamp,
-    },
-    process.env.CLOUDINARY_SECRET
-  );
-  return res.status(201).send({ statusId: 201, timestamp, signature });
-});
+router.post(
+  "/",
+  authController.protect,
+  authController.restrictTo,
+  (req, res) => {
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const signature = cloudinary.utils.api_sign_request(
+      {
+        timestamp: timestamp,
+      },
+      process.env.CLOUDINARY_SECRET
+    );
+    return res.status(201).send({ statusId: 201, timestamp, signature });
+  }
+);
 
 module.exports = router;
